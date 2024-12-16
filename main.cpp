89cafa6b3cf095e87fb765a2728f5304b5c99f3d
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-12-16 10:58:16 +0100

    distributed

--- main.cpp
+++ main.cpp
@@ -41,6 +41,98 @@ public:
     
 };
 
+class DistributedVectorAccountStorage : public IAccountStorage{
+        std::vector<BankAccount> accounts0;
+        std::vector<BankAccount> accounts1;
+        std::vector<BankAccount> accounts2;
+        std::vector<BankAccount> accounts3;
+        std::vector<BankAccount> accounts4;
+        std::vector<BankAccount> accounts5;
+        std::vector<BankAccount> accounts6;
+        std::vector<BankAccount> accounts7;
+        std::vector<BankAccount> accounts8;
+        std::vector<BankAccount> accounts9;
+    
+    public:
+    void addAccount(BankAccount account) override{
+        if(account.getAccountNumber().at(0) == '0'){
+            accounts0.push_back(account);
+        }       
+        else if(account.getAccountNumber().at(0) == '1'){
+            accounts1.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '2'){
+            accounts2.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '3'){
+            accounts3.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '4'){
+            accounts4.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '5'){
+            accounts5.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '6'){
+            accounts6.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '7'){
+            accounts7.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '8'){
+            accounts8.push_back(account);
+        }
+        else if(account.getAccountNumber().at(0) == '9'){
+            accounts9.push_back(account);
+        }
+    }
+
+    BankAccount *findAccount( std::string accountNumber){
+        BankAccount *ret = nullptr;
+        std::vector<BankAccount> &accounts = accounts0;
+
+        if(accountNumber.at(0) == '1'){
+            accounts = accounts1;
+        }
+        if(accountNumber.at(0) == '2'){
+            accounts = accounts2;
+        }
+        if(accountNumber.at(0) == '3'){
+            accounts = accounts3;
+        }
+        if(accountNumber.at(0) == '4'){
+            accounts = accounts4;
+        }
+        if(accountNumber.at(0) == '5'){
+            accounts = accounts5;
+        }
+        if(accountNumber.at(0) == '6'){
+            accounts = accounts7;
+        }
+        if(accountNumber.at(0) == '7'){
+            accounts = accounts7;
+        }
+       if(accountNumber.at(0) == '8'){
+            accounts = accounts8;
+        }
+          if(accountNumber.at(0) == '9'){
+            accounts = accounts9;
+        }
+
+
+        for(BankAccount &account : accounts){
+            if(account.getAccountNumber() == accountNumber ){
+                //ret = &account;      
+                return &account;                                  
+            }
+        }
+        return ret;
+  
+
+    }
+
+};
+
 class VectorAccountStorage: public IAccountStorage{
         std::vector<BankAccount> accounts;
 public:
@@ -89,8 +181,9 @@ public:
 int main(int, char**){
     //VectorAccountStorage storage;
     //VectorAccountStorage storage;
-    MapStorage storage;
-    //MapAccountStorage storage;
+    //MapStorage storage;
+    DistributedVectorAccountStorage storage;
+    //MapAccountStor age storage;
     Bank bank(&storage);
 
     const int AntalAccounts =  10000000;
